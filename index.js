4da06d1530214661982fa2ebfd7db02170dcdfc5
Peter Crawford <pcraw1027@gmail.com>
2021-02-07 20:11:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // add solution here
 var initArray = []
-var i = 0
+var i;
 
 // function theBeatlesPlay(musicians,instruments) {
-//  for () {
+//  for (i = 0, i > musicians.lenght, ) {
   
 //  } 
 
