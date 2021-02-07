5b26b30bfefdde2939b8a4ef8781f03a495c1605
Peter Crawford <pcraw1027@gmail.com>
2021-02-07 19:56:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // add solution here
 var initArray = []
 
-function theBeatlesPlay(musicians,instruments) {
-  for () {
+// function theBeatlesPlay(musicians,instruments) {
+//  for () {
   
-  } 
+//  } 
 
-}
+// }
 
 
 // function johnLennonFacts(lennonArray) {
