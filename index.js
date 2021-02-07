35d0538745f83a1e9652c046730db04897b16cd1
Peter Crawford <pcraw1027@gmail.com>
2021-02-07 20:33:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var initArray = []
 var i;
 
 function theBeatlesPlay(musicians,instruments) {
-  console.log()
+  console.log(musicians.lenght)
   for (i = 0; i < musicians.lenght; i++) {
     initArray.push(musicians[i] + " plays " + instruments[i])
   } 
