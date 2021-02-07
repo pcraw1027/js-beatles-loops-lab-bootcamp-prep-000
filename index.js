c6a7608206fe489e072124685ae6d03605d00ae2
Peter Crawford <pcraw1027@gmail.com>
2021-02-07 20:24:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function theBeatlesPlay(musicians,instruments) {
   for (i = 0; i < musicians.lenght; i++) {
     initArray.push(musicians[i] + " plays " + instruments[i])
   } 
-
+  return initArray
 }
 
 
